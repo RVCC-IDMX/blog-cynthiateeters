26f53976ac04cead4fcf3b61b9c78a24e83b40ff
Cynthia Teeters <cynthiateeters@gmail.com>
2022-10-02 20:51:28 -0400

    Add markdown support
    Add markdown-it and prismJS

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
 const { DateTime } = require('luxon');
+const markdownIt = require("markdown-it");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/admin');
+
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
@@ -12,6 +16,15 @@ module.exports = function (eleventyConfig) {
       'LLLL dd, yyyy '
     );
   });
+
+  // Set options for Markdown-it
+  let options = {
+    html: true, // Enable HTML tags in source
+    breaks: true, // Convert '\n' in paragraphs into <br>
+    linkify: true // Autoconvert URL-like text to links
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(options));
 
   return {
     dir: {
